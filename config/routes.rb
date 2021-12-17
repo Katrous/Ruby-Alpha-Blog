e379dae563da39ed7a953d7f952bad7932e8489a
Katrous <richardslaura24@gmail.com>
2021-12-17 12:42:21 +0800

    articles listing and details complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
-
+  # creates a route for root route, sends to pages controller which returns a view file
   get 'about', to: 'pages#about'
+  # creates a route for the /about url, request is sent to pages controlelr which returns a view file for about
+  resources :articles, only: [:show, :index]
+  # creates routes that work with resources
 end
